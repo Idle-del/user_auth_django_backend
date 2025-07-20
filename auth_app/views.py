a3e5cd63fdb69d48ef0f2a607fe03e2c2da807e9
Idle del <magarkiran436@gmail.com>
2025-07-20 10:05:38 +0545

    Created user auth backend

---
+++ auth_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework import viewsets, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]  # Allow any user to access this view
+    
+    @action(detail=False, methods=['post'], url_path='register')
+    def register(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        return data
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+    permission_classes = [AllowAny]
